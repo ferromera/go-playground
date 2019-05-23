200a2010fc710fdb3cd0b1670963afc15278a4f6
fromera <fernando.romera@mercadolibre.com>
2019-05-23 17:08:39 -0300

    saca comment

--- src/server/server.go
+++ src/server/server.go
@@ -6,7 +6,7 @@ import(
 func New() *gin.Engine {
 	router := gin.Default()
 	teamController := getTeamController()
-	// This handler will match /user/john but will not match /user/ or /user
+	
 	router.GET("/team/:id", teamController.Get)
 	return router
 
